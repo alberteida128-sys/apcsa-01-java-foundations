7f823a11bd505871ddd892ffdd9d86d7d132e9ef
alberteida128-sys <albert_eida128@flatbush.org>
2026-09-17 20:34:23 +0000

    S03 Homework

--- src/HelloYou.java
+++ src/HelloYou.java
@@ -10,8 +10,8 @@
  */
 public class HelloYou {
     public static void main(String[] args) {
-        System.out.println("Albert");
-        System.out.println("11"); 
-        System.out.println("I am a kid who likes sports");
+        System.out.println(" --------");
+        System.out.println(" |Albert|"); 
+        System.out.println(" --------");
     }
 }
